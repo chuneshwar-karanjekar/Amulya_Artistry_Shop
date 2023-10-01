15f96f04cb2d877d1e7bae079a6b1e84a42b4bd0
chuneshwar-karanjekar <chuneshwarkaranjekar@gmail.com>
2023-10-01 20:39:50 +0530

    update' ok

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,8 +1,13 @@
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <h1>Amulya Artistry</h1>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/client/src/pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <Layout>
+      
+    </Layout>
+  )
+}
+
+export default Contact
