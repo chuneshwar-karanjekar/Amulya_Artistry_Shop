554d76d3e0627c7916fa78d79c817e77b814a9d9
chuneshwar-karanjekar <chuneshwarkaranjekar@gmail.com>
2023-10-02 15:58:31 +0530

    new update

--- client/client/src/components/Footer.js
+++ client/client/src/components/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div className="bg-dark text-light p-3">
+    <div className="footer bg-dark text-light p-3">
       <h4 className="text-center">All Right Reserved &copy; Amulya Artistry</h4>
+      <p className="text-center mt-3">
+        <Link to="/about">About</Link>
+        |
+        <Link to="/contact">Contact</Link>
+        |
+        <Link to="/policy">Privacy Policy</Link>
+
+      </p>
     </div>
   );
 };
--- client/client/src/pages/Contact.js
+++ client/client/src/pages/Contact.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Layout from "../components/Layout";
 
 const Contact = () => {
   return (
     <Layout>
-      
+      <h1>Contact page</h1>
     </Layout>
   )
 }
